ae72c7fcfa3f058506d53fd1ed7f45f6f445bac8
Savitri Iyer <138581343+infinitechapters@users.noreply.github.com>
2024-01-11 07:53:19 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,21 @@
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+let inp=document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    let item = document.createElement("li");
+    item.innerText=inp.value;
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+    let btns=document.querySelectorAll(".delete");
+    for(delBtn of btns){
+        btns.addEventListener("click",function(){
+        console.log("deleted");
+    })
+    }
